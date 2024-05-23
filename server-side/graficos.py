e10cf425d425b4f1a7fc85794b7fd95b71627a61
Vinicius Fernandes - fxr <vinni.fernandescar@gmail.com>
2024-05-22 21:31:33 -0300

    final commit

--- server-side/graficos.py
+++ server-side/graficos.py
@@ -8,28 +8,33 @@ def save_plot(plot_func, filename, **kwargs):
     plot_func(**kwargs)
     plt.savefig(filename)
     plt.close()
+    print(f"Gráfico salvo em: {filename}")  # Log para verificar onde o gráfico está sendo salvo
 
 
 def create_all_charts(vetor_embaralhado, vetor_gerado):
     os.makedirs('static/images', exist_ok=True)
 
-    scatter_path = 'static/images/scatter_plot.png'
-    line_path = 'static/images/line_chart.png'
-    bar_path = 'static/images/bar_chart.png'
-    bubble_path = 'static/images/bubble_chart.png'
-    dot_path = 'static/images/dot_plot.png'
+    scatter_path = 'images/scatter_plot.png'
+    line_path = 'images/line_chart.png'
+    bar_path = 'images/bar_chart.png'
+    bubble_path = 'images/bubble_chart.png'
+    dot_path = 'images/dot_plot.png'
 
-    save_plot(sns.scatterplot, scatter_path, x=range(len(vetor_embaralhado)), y=vetor_embaralhado)
-    save_plot(sns.lineplot, line_path, x=range(len(vetor_gerado)), y=vetor_gerado)
-    save_plot(sns.barplot, bar_path, x=range(len(vetor_gerado)), y=vetor_gerado)
-    save_plot(sns.scatterplot, bubble_path, x=range(len(vetor_embaralhado)), y=vetor_embaralhado,
+    print("Criando gráficos...")  # Log para iniciar a criação de gráficos
+
+    save_plot(sns.scatterplot, f'static/{scatter_path}', x=range(len(vetor_embaralhado)), y=vetor_embaralhado)
+    save_plot(sns.lineplot, f'static/{line_path}', x=range(len(vetor_gerado)), y=vetor_gerado)
+    save_plot(sns.barplot, f'static/{bar_path}', x=range(len(vetor_gerado)), y=vetor_gerado)
+    save_plot(sns.scatterplot, f'static/{bubble_path}', x=range(len(vetor_embaralhado)), y=vetor_embaralhado,
               size=[random.randint(1, 100) for _ in range(len(vetor_embaralhado))])
-    save_plot(sns.stripplot, dot_path, x=range(len(vetor_embaralhado)), y=vetor_embaralhado)
+    save_plot(sns.stripplot, f'static/{dot_path}', x=range(len(vetor_embaralhado)), y=vetor_embaralhado)
+
+    print("Gráficos criados.")  # Log para finalizar a criação de gráficos
 
     return {
-        "scatter": 'static/images/scatter_plot.png',
-        "line": 'static/images/line_chart.png',
-        "bar": 'static/images/bar_chart.png',
-        "bubble": 'static/images/bubble_chart.png',
-        "dot": 'static/images/dot_plot.png'
+        "scatter": scatter_path,
+        "line": line_path,
+        "bar": bar_path,
+        "bubble": bubble_path,
+        "dot": dot_path
     }
--- server-side/vetor_json.py
+++ server-side/vetor_json.py
@@ -31,9 +31,19 @@ def generate_vetor_json(tamanho_vetor=1000):
             id INT AUTO_INCREMENT PRIMARY KEY,
             vetor_id INT NOT NULL,
             tempo_embaralhamento FLOAT NOT NULL,
+            tempo_ordenacao FLOAT NOT NULL,
             FOREIGN KEY (vetor_id) REFERENCES vetor(id)
         );
     """)
+
+    # Adiciona a coluna tempo_ordenacao se ela não existir
+    try:
+        cursor.execute("ALTER TABLE tempos_embaralhamento ADD COLUMN tempo_ordenacao FLOAT;")
+    except mysql.connector.Error as err:
+        if err.errno == 1060:  # Duplicate column error
+            pass
+        else:
+            return {"error": f"Erro ao modificar a tabela: {err}"}
 
     def gerar_vetor(tamanho_vetor):
         vetor = list(range(1, tamanho_vetor + 1))
@@ -43,11 +53,23 @@ def generate_vetor_json(tamanho_vetor=1000):
     vetor_gerado = gerar_vetor(tamanho_vetor)
     vetor_embaralhado = vetor_gerado.copy()
     tempos_embaralhamento = []
+    tempos_ordenacao = []
+
     for _ in range(3):
+        # Tempo de embaralhamento
         start_time = time.time()
         embaralhar_fisher_yates(vetor_embaralhado)
         end_time = time.time()
         tempos_embaralhamento.append(end_time - start_time)
+
+        # Tempo de ordenação
+        start_time = time.time()
+        vetor_embaralhado.sort()
+        end_time = time.time()
+        tempos_ordenacao.append(end_time - start_time)
+
+        # Embaralhar novamente para a próxima interação
+        random.shuffle(vetor_embaralhado)
 
     vetor_gerado.sort()
 
@@ -66,11 +88,11 @@ def generate_vetor_json(tamanho_vetor=1000):
 
     vetor_id = cursor.lastrowid
 
-    for tempo in tempos_embaralhamento:
+    for tempo_emb, tempo_ord in zip(tempos_embaralhamento, tempos_ordenacao):
         cursor.execute("""
-            INSERT INTO tempos_embaralhamento (vetor_id, tempo_embaralhamento)
-            VALUES (%s, %s)
-        """, (vetor_id, tempo))
+            INSERT INTO tempos_embaralhamento (vetor_id, tempo_embaralhamento, tempo_ordenacao)
+            VALUES (%s, %s, %s)
+        """, (vetor_id, tempo_emb, tempo_ord))
 
     conn.commit()
     cursor.close()
@@ -84,6 +106,7 @@ def generate_vetor_json(tamanho_vetor=1000):
         "vetor_aleatorio": vetor_embaralhado,
         "vetor_ordenado": vetor_gerado,
         "tempos_embaralhamento": tempos_embaralhamento,
+        "tempos_ordenacao": tempos_ordenacao,
         "graficos": graficos
     }
 
